bfb6167ef0ae090bf11bee41a7c28a847ee9974a
LutherMckeiver <leoluther96@gmail.com>
2019-01-08 07:41:46 -0800

    Finished Question 1

--- AboutMeQuiz/AboutMeQuiz/Program.cs
+++ AboutMeQuiz/AboutMeQuiz/Program.cs
@@ -7,7 +7,29 @@ namespace AboutMeQuiz
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            QuestionOne();
             Console.ReadLine();
+        }
+
+        static string QuestionOne()
+        {
+            int statesVisited = 18;
+            string replyCorrect = "You're correct I have visited 18 states";
+            string replyWrong = "Sorry Wrong Answer";
+            Console.WriteLine("How many states have I visited?");
+            string userResponse = Console.ReadLine();
+            int formattedUserResponse = int.Parse(userResponse);
+
+            if (formattedUserResponse == statesVisited)
+            {
+                Console.WriteLine(replyCorrect);
+                return replyCorrect;
+            }
+            else
+            {
+                Console.WriteLine(replyWrong);
+                return replyWrong;
+            }
         }
     }
 }
